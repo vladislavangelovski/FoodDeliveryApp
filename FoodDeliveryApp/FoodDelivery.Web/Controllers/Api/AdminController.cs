81f304e773888c1f551b68e861e256fe3afb64e3
Vladislav Angelovski <angelovski2505@gmail.com>
2024-12-08 16:39:25 +0100

    Changed folder structure and apiAdmin controller

---
+++ FoodDeliveryApp/FoodDelivery.Web/Controllers/Api/AdminController.cs
@@ -0,0 +1,24 @@
+﻿using FoodDelivery.Domain.DomainModels;
+using FoodDelivery.Service.Interface;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FoodDelivery.Web.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminController : ControllerBase
+    {
+        private readonly IRestaurantService _restaurantService;
+
+        public AdminController(IRestaurantService restaurantService)
+        {
+            _restaurantService = restaurantService;
+        }
+
+        [HttpGet("[action]")]
+        public List<Restaurant> GetAllRestaurants() { 
+            return _restaurantService.GetRestaurants();
+        }
+    }
+}
