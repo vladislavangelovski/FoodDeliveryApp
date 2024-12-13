17e7251d3b14373a7bf13f4b6ead5f952a6daf3a
Hristina Stojanovska <hristina.nina02@gmail.com>
2024-12-13 03:45:57 +0100

    Created ImportRestaurants()

--- FoodDeliveryAdminApp/FoodDeliveryAdminApp/Controllers/RestaurantController.cs
+++ FoodDeliveryAdminApp/FoodDeliveryAdminApp/Controllers/RestaurantController.cs
@@ -1,4 +1,7 @@
 ﻿using System.Text;
+using DocumentFormat.OpenXml.Spreadsheet;
+using DocumentFormat.OpenXml.Wordprocessing;
+using ExcelDataReader;
 using FoodDeliveryAdminApp.Models;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
@@ -37,6 +40,103 @@ namespace FoodDeliveryAdminApp.Controllers
 
 
             return View(data);
+
+        }
+
+        public IActionResult ImportIndex()
+        {
+            return View();
+        }
+
+        public IActionResult ImportRestaurants(IFormFile file)
+        {
+            string pathToUpload = $"{Directory.GetCurrentDirectory()}\\files\\{file.FileName}";
+
+            using (FileStream fileStream = System.IO.File.Create(pathToUpload))
+            {
+                file.CopyTo(fileStream);
+                fileStream.Flush();
+            }
+
+            List<Restaurant> restaurants = getAllRestaurantsFromFile(file.FileName);
+
+            HttpClient client = new HttpClient();
+            string URL = "http://localhost:5070/api/Admin/ImportRestaurants";
+
+            HttpContent content = new StringContent(JsonConvert.SerializeObject(restaurants), Encoding.UTF8, "application/json");
+
+            HttpResponseMessage response = client.PostAsync(URL, content).Result;
+
+            var result = response.Content.ReadAsAsync<bool>().Result;
+
+            return RedirectToAction("Index", "Restaurant");
+        }
+
+        private List<Restaurant> getAllRestaurantsFromFile(string fileName)
+        {
+            List<Restaurant> restaurants = new List<Restaurant>();
+            string filePath = $"{Directory.GetCurrentDirectory()}\\files\\{fileName}";
+
+            System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+
+            using (var stream = System.IO.File.Open(filePath, FileMode.Open, FileAccess.Read))
+            {
+                using (var reader = ExcelReaderFactory.CreateReader(stream))
+                {
+                    while (reader.Read())
+                    {
+                        var restaurant = restaurants.FirstOrDefault(r => r.Name == reader.GetValue(0).ToString());
+
+                        if (restaurant == null)
+                        {
+                            restaurant = new Models.Restaurant
+                            {
+                                Name = reader.GetValue(0).ToString(),
+                                Address = reader.GetValue(1).ToString(),
+                                PhoneNumber = reader.GetValue(2).ToString(),
+                                Description = reader.GetValue(3).ToString(),
+                                Image = reader.GetValue(4).ToString(),
+                                Rating = Convert.ToDouble(reader.GetValue(5)),
+                                FoodItems = new List<FoodItem>(),
+                                Ratings = new List<Rating>(),
+                            };
+
+                            restaurants.Add(restaurant);
+                        }
+
+                        int i = 6;
+                        while(i < reader.FieldCount && reader.GetValue(i) != null && reader.GetValue(i).ToString() != "")
+                        {
+                            if( i+5 < reader.FieldCount)
+                            {
+                                var fooditem = new Models.FoodItem
+                                {
+                                    Name = reader.GetValue(i).ToString(),
+                                    Price = Convert.ToDouble(reader.GetValue(i + 1)),
+                                    Description = reader.GetValue(i + 2).ToString(),
+                                    Image = reader.GetValue(i + 3).ToString(),
+                                    Category = reader.GetValue(i + 4).ToString(),
+                                    RestaurantId = restaurant.Id,
+                                    TimeToPrepareMinutes = (int)Convert.ToDouble(reader.GetValue(i + 5)),
+                                    FoodItemInDeliveries = new List<FoodItemInDelivery>(),
+                                    FoodItemInOrders = new List<FoodItemInOrder>()
+                                };
+
+                                restaurant.FoodItems.Add(fooditem);
+                             
+                            }
+                            else
+                            {
+                                break;
+                            }
+                            i += 6;
+                        }
+                      
+                    }
+
+                }
+            }
+            return restaurants;
 
         }
     }
--- FoodDeliveryApp/FoodDelivery.Web/Controllers/Api/AdminController.cs
+++ FoodDeliveryApp/FoodDelivery.Web/Controllers/Api/AdminController.cs
@@ -1,6 +1,7 @@
 ﻿using FoodDelivery.Domain.DomainModels;
 using FoodDelivery.Service.Interface;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FoodDelivery.Web.Controllers.Api
@@ -11,12 +12,14 @@ namespace FoodDelivery.Web.Controllers.Api
     {
         private readonly IRestaurantService _restaurantService;
         private readonly IOrderService _orderService;
+        private readonly IFoodItemService _foodItemService;
        
 
-        public AdminController(IRestaurantService restaurantService, IOrderService orderService)
+        public AdminController(IRestaurantService restaurantService, IOrderService orderService, IFoodItemService foodItemService)
         {
             _restaurantService = restaurantService;
             _orderService = orderService;
+            _foodItemService = foodItemService;
         }
 
         [HttpGet("[action]")]
@@ -41,6 +44,63 @@ namespace FoodDelivery.Web.Controllers.Api
         public Order GetOrderDetails(BaseEntity id)
         {
             return this._orderService.GetOrderDetails(id);
+        }
+
+        [HttpPost("[action]")]
+        public bool ImportRestaurants(List<Restaurant> model)
+        {
+            bool status = true;
+
+            foreach (var item in model)
+            {
+                var restaurant = new Restaurant
+                {
+                    Name = item.Name,
+                    Address = item.Address,
+                    PhoneNumber = item.PhoneNumber,
+                    Description = item.Description,
+                    Image = item.Image,
+                    Rating = item.Rating,
+                    FoodItems = new List<FoodItem>(),
+                    Ratings = item.Ratings,               
+                };
+
+                var result = _restaurantService.CreateNewRestaurant(restaurant);
+
+                if(result != null)
+                {
+                    var foodItems = item.FoodItems ?? new List<FoodItem>();
+                    if (item.FoodItems != null)
+                    {
+                        foreach (var tmp in item?.FoodItems)
+                        {
+                            if(tmp != null)
+                            {
+                                var fooditem = new FoodItem
+                                {
+                                    Name = tmp.Name,
+                                    Price = tmp.Price,
+                                    Description = tmp.Description,
+                                    Image = tmp.Image,
+                                    Category = tmp.Category,
+                                    RestaurantId = result.Id,
+                                    TimeToPrepareMinutes = tmp.TimeToPrepareMinutes,
+                                    Restaurant = result,
+                                    FoodItemInDeliveries = new List<FoodItemInDelivery>(),
+                                    FoodItemInOrders = new List<FoodItemInOrder>(),
+                                };
+
+                                var result1 = _foodItemService.CreateNewFoodItem(fooditem);
+                                if(result1 != null)
+                                    restaurant.FoodItems.Add(fooditem);
+                            }
+                        }
+                    }
+                }
+                status = status && (result != null); 
+
+            }
+            return status;
         }
     }
 }
